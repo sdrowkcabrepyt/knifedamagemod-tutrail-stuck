99ee5da4acad72ef5be2d1cdcce7155138973bb5
Lets game it out <136708748+sdrowkcabrepyt@users.noreply.github.com>
2023-12-17 03:46:52 +1100

    Add project files.

---
+++ knifedamagemod/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("knifedamagemod")]
+[assembly: AssemblyDescription("A Subnautica mod to incrase the damage of the knife")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Infaman Gaming")]
+[assembly: AssemblyProduct("knifedamagemod")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d130809-4eca-4991-9dbb-7fd1e9dfebec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
